4be5e07f680d1f75346ec0600961aa8481b0cac6
DanRobo76 <102819027+DanRobo76@users.noreply.github.com>
2024-12-01 09:32:24 +0100

    Delete vmc_master_script_cambio_ip.sh

--- vmc_master_script_cambio_ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-IP="$1"  # Imposta l'indirizzo IP specificato come argomento
-
-# Esegui la sostituzione dell'indirizzo IP nel file YAML
-sed -i "s/[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/$IP/g" "/config/packages/vmc_helty_flow/master/vmc_master.yaml"
-sed -i 's/nc [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+ 5001/nc '"$IP"' 5001/' "/config/packages/vmc_helty_flow/master/vmc_master_script_cambio_nome.sh"
